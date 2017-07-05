7bc96cafc940f747d884ac7e4b787d2d3b30a8a8
mitki <mitki@hacari.org>
2017-07-05 12:06:34 -0300

    Show more JAP -> ENG definitions

--- modules/commands/japanese.rb
+++ modules/commands/japanese.rb
@@ -7,13 +7,17 @@ module Cygnus
 
       require "mojinizer"
 
-      command :jp, description: "Dicionário Japonês > Inglês." do |event, arg|
-        jp = JSON.parse( open( "http://jisho.org/api/v1/search/words?keyword=#{arg}" ).read )
+      command :jp, description: "Dicionário Japonês > Inglês." do |event, word, def_number = 1|
+        jp = JSON.parse( open( "http://jisho.org/api/v1/search/words?keyword=#{word}" ).read )
 
-        # First result only.
-        kanji   = jp["data"][0]["japanese"][0]["word"]
-        reading = jp["data"][0]["japanese"][0]["reading"]
-        english = jp["data"][0]["senses"][0]["english_definitions"]
+        def_number = 1 if def_number.to_i == 0
+        def_number = def_number.to_i - 1
+
+        # !jp kami   -> mostra a 1ª definição [0].
+        # !jp kami 2 -> mostra a 2ª definição [1].
+        kanji   = jp["data"][def_number]["japanese"][0]["word"]
+        reading = jp["data"][def_number]["japanese"][0]["reading"]
+        english = jp["data"][def_number]["senses"][0]["english_definitions"]
 
         event.channel.send_embed do |embed|
           embed.add_field name: "Kanji", value: kanji, inline: true
@@ -36,6 +40,16 @@ module Cygnus
           embed.add_field name: "Original", value: args.join(" "), inline: true
           embed.add_field name: "Romaji (Estimativa)", value: romaji[0], inline: true
         end
+      end
+
+      command :jp_info, description: "Informações sobre a língua japonesa." do |event|
+        event << "```"
+        event << "http://jisho.org/"
+        event << "http://www.guidetojapanese.org/learn/grammar"
+        event << "http://maggiesensei.com/"
+        event << "https://www.youtube.com/user/freejapaneselessons3"
+        event << "https://ankiweb.net/shared/decks/japanese"
+        event << "```"
       end
     end
   end
