cd1759cf85dae617dd1a1ca6bf37b6d203e3c4b1
mitki <mitki@hacari.org>
2017-07-07 15:42:01 -0300

    Core reescrito

--- nyaa.rb
+++ nyaa.rb
@@ -1,41 +1,31 @@
 require "discordrb"       # Discord API.
-require "open3"           # Prevent shell injection.
-require "nokogiri"        # HTTP scraping.
-require "open-uri"        # HTTP scraping.
-require "sqlite3"         # Database.
 require "yaml"            # Config Files.
-require 'json'            # Parse json files.
 
-# IMPORTANT!
-DIR = File.dirname(__FILE__)
-DB_DIR = "#{DIR}/database"
-DATA_DIR = "#{DIR}/data"
-CONFIG = YAML.load_file("#{DIR}/config/config.yml")
+# GLOBAL CONSTANTS.
+DIR      =  File.dirname(__FILE__)
+DIR_DB   =  DIR+"/database"
+DIR_DATA =  DIR+"/data"
+DIR_LOG  =  DIR_DATA+"/logs"
+CONFIG   =  YAML.load_file DIR+"/config/config.yml"
 
 module Cygnus
   # Inclui todos os arquivos do diretório /modules
   # em seu determinado módulo.
 
-  module Cygnus_Commands
-    Dir["#{DIR}/modules/commands/*.rb"].each { |file| require file }
-  end
-
-  module Cygnus_Events
-    Dir["#{DIR}/modules/events/*.rb"].each { |file| require file }
-  end
-
   bot = Discordrb::Commands::CommandBot.new token: CONFIG["token"], client_id: CONFIG["client_id"], prefix: CONFIG["prefix"]
 
-  # EVENT.
-  bot.include! Cygnus_Events::Version
-  bot.include! Cygnus_Events::User_Management
+  # Importa os módulos e inclui cada constante.
+  Dir["#{DIR}/modules/*.rb"].each           { |file| require file }
+  Dir["#{DIR}/modules/commands/*.rb"].each  { |file| require file }
+  Dir["#{DIR}/modules/events/*.rb"].each    { |file| require file }
 
-  # COMMAND.
-  bot.include! Cygnus_Commands::Administration
-  bot.include! Cygnus_Commands::User_Info
-  bot.include! Cygnus_Commands::EcchiNyaa
-  bot.include! Cygnus_Commands::Japanese
-  bot.include! Cygnus_Commands::Help
+  Cygnus_Commands.constants.each do |command|
+    bot.include! Cygnus_Commands.const_get command
+  end
+
+  Cygnus_Events.constants.each do |event|
+    bot.include! Cygnus_Events.const_get event
+  end
 
   bot.run
 end
