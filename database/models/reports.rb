adb649d83df3dfa10a90b1188479397e1bd307b6
mitki <mitki@hacari.org>
2017-07-10 19:59:00 -0300

    Modelos atualizados

--- database/models/reports.rb
+++ database/models/reports.rb
@@ -6,10 +6,6 @@ module Cygnus
     Sequel::Migrator.run REPORTS, "#{DIR_DB}/migrations/reports"
 
     class Report < Sequel::Model REPORTS[:reports]
-      def after_save
-        self.updated_on = Time.now
-      end
-
       def show
         channel = BOT.server( server_id ).channels.find { |ch| ch.id == CONFIG["admin_channel_id"] }
 
@@ -18,10 +14,21 @@ module Cygnus
           embed.color = "7EC0EE"
           embed.description = "Use \"!ticket ##{id} fechar\" para fechar o ticket."
           embed.add_field name: "Usuário", value: user, inline: true
-          embed.add_field name: "Status", value: status == 1 ? "ABERTO" : "Fechado", inline: true if updated_on.nil?
+          embed.add_field name: "Status", value: "ABERTO" if status == 1
           embed.add_field name: "Data da abertura", value: created_on.strftime( "%d/%m/%Y %H:%M" ), inline:true
           embed.add_field name: "Concluído em:", value: updated_on.strftime( "%d/%m/%Y %H:%M" ), inline: true unless updated_on.nil?
           embed.add_field name: "Conteúdo", value: content
+        end
+      end
+
+      def fechar
+        self.update status: 0,
+                    updated_on: Time.now
+      end
+
+      dataset_module do
+        def autolimpeza
+          where{ Time.now - created_on >= 24 * 3600 * 7 }
         end
       end
     end
