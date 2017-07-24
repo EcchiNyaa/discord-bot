ede251d4cddff6a4f06d1620313673c59db5688b
mitki <mitki@hacari.org>
2017-07-24 11:31:12 -0300

    Protótipo do módulo de estatísticas

---
+++ modules/events/stats.rb
@@ -0,0 +1,30 @@
+module Cygnus
+  module Cygnus_Events
+    # MÓDULO DE ESTATÍSTICAS
+    # Exibe as estatísticas do servidor, como total de comandos efetuados e mensagens por dia.
+    module Stats
+      extend Discordrb::EventContainer
+
+      ready do |event|
+        event.bot.servers.values.map(&:id).each do |server|
+          db = Cygnus::Database::Stats
+          next unless db.where( server_id: server ).count.zero?
+
+          db.create( server_id: server )
+        end
+      end
+
+      message do |event|
+        db = Cygnus::Database::Stats.where( server_id: event.server.id ).first
+        db.messages += 1
+        db.save
+
+        next unless event.message.content.start_with? CONFIG["prefix"]
+
+        db = Cygnus::Database::Stats.where( server_id: event.server.id ).first
+        db.commands += 1
+        db.save
+      end
+    end
+  end
+end
